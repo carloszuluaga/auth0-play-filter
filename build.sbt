8db6a734a3eede0f2fb8d1f0dee28024dcca788d
Carlos Zuluaga <carloszuluaga@seven4n.com>
2016-02-16 20:50:49 -0500

    First commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := """auth0-play-filter"""
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.play" %% "play" % "2.4.6",
+  "com.typesafe" % "config" % "1.3.0",
+  "com.auth0" % "java-jwt" % "2.1.0",
+  "commons-codec" % "commons-codec" % "1.4",
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test")
---
+++ src/main/scala/co/s4n/play/filters/Auth0Filter.scala
@@ -0,0 +1,58 @@
+package co.s4n.play.filters
+
+import play.api.Logger
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import com.auth0.jwt.JWTVerifier
+import org.apache.commons.codec.binary.Base64
+import scala.util.Try
+import com.typesafe.config.ConfigFactory
+
+class Auth0Filter extends Filter {
+
+  lazy val config = ConfigFactory.load()
+  lazy val clientId = config.getString("auth0.clientId")
+  lazy val clientSecret = config.getString("auth0.clientSecret")
+  lazy val jwtVerifier = new JWTVerifier(new Base64(true).decode(clientSecret), clientId)
+
+  def apply(nextFilter: RequestHeader => Future[Result])
+           (requestHeader: RequestHeader): Future[Result] = {
+
+    if(requestHeader.method == "OPTIONS"){
+      nextFilter(requestHeader)
+    }else{
+      val tokenEither = getToken(requestHeader)
+      tokenEither match {
+        case Right(token) =>
+          val tokenTry = Try{
+            jwtVerifier.verify(token)
+            nextFilter(requestHeader)
+          }.recover{
+            case e => Future(Unauthorized(e.getMessage))
+          }
+          tokenTry.get
+        case Left(error) =>
+          Future(Unauthorized(error))
+      }
+    }
+  }
+
+  private def getToken(requestHeader: RequestHeader): Either[String,String] = {
+    val authorizationHeaderOpt = requestHeader.headers.get("authorization")
+    authorizationHeaderOpt.fold[Either[String, String]](Left("Unauthorized: No Authorization header was found")){authorizationHeader =>
+      val parts = authorizationHeader.split(" ")
+      if(parts.size != 2){
+        Left("Unauthorized: Format is Authorization: Bearer [token]")
+      }else{
+        val scheme = parts(0)
+        val credentials = parts(1)
+        scheme match {
+          case "Bearer" => Right(credentials)
+          case _ => Left("Unauthorized: Format is Authorization: Bearer [token]")
+        }
+      }
+    }
+  }
+}
